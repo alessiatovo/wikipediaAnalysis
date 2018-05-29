f7a882f31a4c606d87ee01e7e834122dd2b18828
Alessia <alessiatovo@users.noreply.github.com>
2018-05-29 16:08:11 +0200

    runnable script

--- main.sh
+++ main.sh
@@ -1,3 +1,5 @@
+#! /bin/bash
+
 spark-submit --class mainclasses.App target/graphCreation-1.0-SNAPSHOT-jar-with-dependencies.jar hdfs://localhost:9000/user/alessia/ML/input hdfs://localhost:9000/user/alessia/ML/output/ /home/alessia/UNI/BIG_DATA/hadoop-2.8.3/bin/hdfs
 spark-submit --class mainclasses.LinkGraph target/graphCreation-1.0-SNAPSHOT-jar-with-dependencies.jar hdfs://localhost:9000/user/alessia/ML/output/
 spark-submit --class mainclasses.WordcountGraph target/graphCreation-1.0-SNAPSHOT-jar-with-dependencies.jar hdfs://localhost:9000/user/alessia/ML/output/
